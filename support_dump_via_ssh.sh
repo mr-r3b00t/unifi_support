8fcc130ed70d50ee554258fead3262d23c1f6889
Daniel Card <dan.card@xservus.com>
2026-03-26 19:24:02 +0000

    Create support_dump_via_ssh.sh

---
+++ support_dump_via_ssh.sh
@@ -0,0 +1,18 @@
+# 1. Collect UniFi Core logs
+tar -zcvf unifi-core-logs.tar.gz /data/unifi-core/logs/
+
+# 2. Collect ULP-Go logs
+tar -zcvf ulp-go-logs.tar.gz /data/ulp-go/log/
+
+# 3. Generate system support dump, then tar it
+ubnt-systool support /tmp/system
+tar -zcvf system.tar.gz /tmp/system
+
+# 4. Bundle everything into a single timestamped archive
+tar -zcvf "$(date +%Y%m%d-%H%M%S)-support.tar.gz" \
+  unifi-core-logs.tar.gz \
+  ulp-go-logs.tar.gz \
+  system.tar.gz
+
+# 5. Cleanup intermediary files (optional)
+rm -f unifi-core-logs.tar.gz ulp-go-logs.tar.gz system.tar.gz
