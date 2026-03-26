baf02753074b82c28e723c7568ad9c645e4432d2
Daniel Card <dan.card@xservus.com>
2026-03-26 19:32:54 +0000

    Update SSH console IP address for backups

--- download_backups_ssh.sh
+++ download_backups_ssh.sh
@@ -1,3 +1,3 @@
-CONSOLE=root@192.168.1.1
+CONSOLE=root@192.168.0.1
 
-ssh "$CONSOLE" 'tar -zcvf /tmp/$(date +%Y%m%d-%H%M%S)-backups.tar.gz /data/unifi/data/backup/ /data/unifi-core/backups/' && scp "$CONSOLE":/tmp/*-backups.tar.gz .
+ssh "$CONSOLE" 'tar -zcvf /tmp/$(date +%Y%m%d-%H%M%S)-backups.tar.gz /data/unifi/data/backup/ /data/unifi-core/backups/' && scp "$CONSOLE":"/tmp/*-backups.tar.gz" .
